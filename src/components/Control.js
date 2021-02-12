d3658fae1ec0b008e41fe000af9808232ad4474b
Eric Endsley <eric.endsley4@gmail.com>
2021-02-12 09:26:53 -0800

    add anticipated components, code keg component

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function Control () {
+class KegControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage:false,
+      masterKegList: [],
+      selectedKeg: null
+    }
+  }
+  
   return( 
     <React.Fragment>
       <h1>Control goes here</h1>
     </React.Fragment>
-  )
+  );
 }
 
-export default Control;
+export default KegControl;
---
+++ src/components/Keg.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+  return (
+    <React.Fragment>
+      <p>Name: {props.name}</p>
+      <p>Brand: {props.brand}</p>
+      <p>Price: {props.price}</p>
+      <p>ABV: {props.abv}</p>
+      <p>Pints Remaining: {props.pints}</p>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  pintsLeft: PropTypes.number,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
+};
