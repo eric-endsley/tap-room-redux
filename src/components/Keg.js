231376de90ac7553edf8787fdb7c4d78c24b940c
Eric Endsley <eric.endsley4@gmail.com>
2021-02-12 13:33:58 -0800

    add border to keg cards & click handler

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,13 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
+  const kegCardStyles = {
+    border: 'solid',
+    marginRight: '50%',
+    marginBottom: '1rem'
+  }
   return (
     <React.Fragment>
+      <div style={kegCardStyles} onClick= {() => props.whenKegCardClicked(props.id)}>  
       <p>Name: {props.name}</p>
       <p>Brand: {props.brand}</p>
       <p>Price: {props.price}</p>
       <p>ABV: {props.abv}</p>
       <p>Pints Remaining: {props.pints}</p>
+      </div>
     </React.Fragment>
   );
 }
