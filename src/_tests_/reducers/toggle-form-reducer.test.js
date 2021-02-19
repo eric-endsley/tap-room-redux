8e64911c75d6a31a7b326f2579dbab907c7fd66c
Eric Endsley <eric.endsley4@gmail.com>
2021-02-19 13:27:38 -0800

    write first test of toggle form reducer and fail it

--- src/_tests_/reducers/toggle-form-reducer.test.js
+++ src/_tests_/reducers/toggle-form-reducer.test.js
@@ -1,2 +1,10 @@
-// import toggleFormReducer from '../../reducers/toggle-form-reducer';
+import toggleFormReducer from '../../reducers/toggle-form-reducer';
+import * as a from '../../actions/index'
 
+describe("toggleFormReducer", () => {
+  test('Should return default state if no action type is recognized', () => {
+    expect(toggleFormReducer("", {type: null})).toEqual(false)
+  });
+
+
+});
