595268be3caded28a676797f06da366079489060
Eric Endsley <eric.endsley4@gmail.com>
2021-02-19 14:40:52 -0800

    write forgotten 3rd test for rootReducer, cofirm pass

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,7 +1,7 @@
 import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
+import * as a from '../../actions/index';
 import toggleFormReducer from '../../reducers/toggle-form-reducer';
-
 let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
@@ -13,5 +13,11 @@ describe("rootReducer", () => {
   
   test("check that initial state of toggleFormReducer matches root reducer", () => {
     expect(store.getState().formVisibleOnPage).toEqual(toggleFormReducer(undefined, { type: null }));
+  });
+
+  test('check that TOGGLE_FORM action works for rootReducer', () => {
+    const action = a.toggleForm();
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(toggleFormReducer(undefined, action));
   });
 });
--- src/_tests_/reducers/toggle-form-reducer.test.js
+++ src/_tests_/reducers/toggle-form-reducer.test.js
@@ -3,7 +3,7 @@ import * as a from '../../actions/index'
 
 describe("toggleFormReducer", () => {
   test('Should return initial state if no action type is recognized', () => {
-    expect(toggleFormReducer(false, {type: null})).toEqual(false)
+    expect(toggleFormReducer(undefined, {type: null})).toEqual(false)
   });
 
   test('Should return opposite state if toggle form action is passed', () => {
