fa36caad9c3f19d919041b0829727af12f68bf66
Eric Endsley <eric.endsley4@gmail.com>
2021-02-19 14:14:10 -0800

    pass test to verify rootReducer default state for form

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,9 +1,17 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import toggleFormReducer from '../../reducers/toggle-form-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       formVisibleOnPage: false
-    })
-  })
-})
+    });
+  });
+  
+  test("check that initial state of toggleFormReducer matches root reducer", () => {
+    expect(store.getState().formVisibleOnPage).toEqual(toggleFormReducer(undefined, { type: null }));
+  });
+});
