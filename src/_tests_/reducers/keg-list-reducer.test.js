df7de0b06111d011c1050cb9a5555dfd5f403e3e
Eric Endsley <eric.endsley4@gmail.com>
2021-02-19 16:03:03 -0800

    write 2nd test of keg list reducer

---
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -0,0 +1,53 @@
+import kegListReducer from '../../reducers/keg-list-reducer';
+import * as a from '../../actions/index'
+
+
+describe('kegListReducer', () => {
+
+  const preLoadStateObj = {
+    1: {
+      name: "Normal Beer",
+      brand: "Normal Beer Brewing",
+      price: 5,
+      abv: 5.0,
+      pints: 124,
+      id: 1
+    },
+  }
+  
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(kegListReducer(undefined, {type: null})).toEqual(preLoadStateObj);
+  });
+
+  test('Should add the keg passed as an argument if addKeg action is passed', () => {
+    const newKeg = {
+      name: "AbNormal Beer",
+      brand: "AbNormal Beer Brewing",
+      price: 5,
+      abv: 5.0,
+      pints: 124,
+      id: 2
+    }
+  
+    const changedState = {
+      1: {
+        name: "Normal Beer",
+        brand: "Normal Beer Brewing",
+        price: 5,
+        abv: 5.0,
+        pints: 124,
+        id: 1
+      },
+      2: {
+        name: "AbNormal Beer",
+        brand: "AbNormal Beer Brewing",
+        price: 5,
+        abv: 5.0,
+        pints: 124,
+        id: 2
+      }  
+    }
+
+    expect(kegListReducer(undefined, a.addKeg(newKeg).toEqual(changedState) ))
+  });
+});
